052f63f0b0e15bd07570eb4d645d67f7e0a66eae
RedBomb1811 <dimadudko00@gmail.com>
2018-10-10 23:24:18 +0300

    Client and Server

--- server.js
+++ server.js
@@ -1,19 +1,38 @@
 const net = require('net');
+const fs = require('fs');
 const port = 8124;
+let seed = 0;
+let right = 0;
 
 const server = net.createServer((client) => {
-    console.log('Client connected');
-
+    const logger = fs.createWriteStream('client_' + seed + '.log');
+    logger.write('Client ' + seed + ' connected\n');
+    client.id = seed++;
     client.setEncoding('utf8');
 
     client.on('data', (data) => {
-        console.log(data);
-        client.write('\r\nHello!\r\nRegards,\r\nServer\r\n');
+        dataHandler(data, client, logger)
     });
 
-    client.on('end', () => console.log('Client disconnected'));
+    client.on('end', () => logger.write('Client ' + client.id + ' disconnected'));
 });
 
 server.listen(port, () => {
     console.log(`Server listening on localhost:${port}`);
 });
+
+function dataHandler(data, client, logger) {
+    if (data === 'QA') {
+        client.write("ACK");
+        right = 0;
+    }
+    else {
+        logger.write('Question: ' + data);
+        let answer = Math.floor(Math.random() * 2);
+        if (answer === 1)
+            right++;
+        logger.write('\nAnswer: ' + answer + '\n');
+        logger.write('Statistic: ' + right + ' right answer\n')
+        client.write(answer);
+    }
+}
